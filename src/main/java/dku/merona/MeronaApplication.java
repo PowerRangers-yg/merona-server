5e3ba3db12d05edfcae217b1a91d7c29184acd9b
kys0411 <32180846@dankook.ac.kr>
2023-03-04 18:02:06 +0900

    add: create project

---
+++ src/main/java/dku/merona/MeronaApplication.java
@@ -0,0 +1,13 @@
+package dku.merona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeronaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeronaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dku/merona/MeronaApplicationTests.java
@@ -0,0 +1,13 @@
+package dku.merona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeronaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
