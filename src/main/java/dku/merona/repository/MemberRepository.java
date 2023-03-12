416f2377daf4c545ec37dc597a81bf2f6c50035f
kys0411 <32180846@dankook.ac.kr>
2023-03-12 18:12:50 +0900

    add: member Repository 추가

---
+++ src/main/java/dku/merona/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package dku.merona.repository;
+
+import dku.merona.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
