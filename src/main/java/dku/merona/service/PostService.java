1a89c46dc8f5a541e3edc90df27bd511e732e267
kys0411 <32180846@dankook.ac.kr>
2023-03-13 16:23:06 +0900

    add: 멤버 검증 코드 추가

--- src/main/java/dku/merona/service/PostService.java
+++ src/main/java/dku/merona/service/PostService.java
@@ -1,5 +1,7 @@
 package dku.merona.service;
 
+import dku.merona.config.UserDetailsImpl;
+import dku.merona.domain.Member;
 import dku.merona.domain.Post;
 import dku.merona.dto.PostRequest;
 import dku.merona.dto.PostResponse;
@@ -16,13 +18,16 @@ import java.util.NoSuchElementException;
 public class PostService {
 
     private final PostRepository postRepository;
+    private final MemberService memberService;
 
     public Post findPostById(Long postId) {
         return postRepository.findById(postId)
                 .orElseThrow(() -> new NoSuchElementException("게시물이 없습니다"));
     }
 
-    public PostResponse createPost(PostRequest postRequest) {
+    public PostResponse createPost(PostRequest postRequest, UserDetailsImpl user) {
+        Member member = memberService.findMemberById(user.getId());
+        postRequest.setMember(member);
         Post post = postRepository.save(postRequest.toEntity());
         return new PostResponse(post);
     }
@@ -32,13 +37,23 @@ public class PostService {
         return new PostResponse(post);
     }
 
-    public PostResponse updatePost(Long postId, PostRequest postRequest) {
+    public PostResponse updatePost(Long postId, PostRequest postRequest, UserDetailsImpl user) {
         Post post = findPostById(postId);
+        isEqualMember(user, post.getMember());
         post.setPost(postRequest);
         return new PostResponse(post);
     }
 
-    public void deletePost(Long postId) {
+    public void deletePost(Long postId, UserDetailsImpl user) {
+        Post post = findPostById(postId);
+        isEqualMember(user, post.getMember());
         postRepository.deleteById(postId);
+    }
+
+    public boolean isEqualMember(UserDetailsImpl user, Member member) {
+        if (!user.getId().equals(member.getId())) {
+            throw new IllegalStateException("잘못된 요청입니다");
+        }
+        return true;
     }
 }
