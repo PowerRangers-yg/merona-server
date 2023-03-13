7215927c1826c98afd66c97210e211d744e41ed6
kys0411 <32180846@dankook.ac.kr>
2023-03-13 16:21:00 +0900

    add: 권한이 필요한 요청에 Authentication 어노테이션 추가

--- src/main/java/dku/merona/api/PostController.java
+++ src/main/java/dku/merona/api/PostController.java
@@ -1,23 +1,28 @@
 package dku.merona.api;
 
+import dku.merona.config.UserDetailsImpl;
 import dku.merona.dto.PostRequest;
 import dku.merona.dto.PostResponse;
 import dku.merona.service.PostService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/posts")
 @RequiredArgsConstructor
+@Slf4j
 public class PostController {
 
     private final PostService postService;
 
     @PostMapping("/new")
-    public void createPost(@RequestBody PostRequest request) {
-        postService.createPost(request);
+    public ResponseEntity<PostResponse> createPost(@AuthenticationPrincipal UserDetailsImpl user,
+                                                   @RequestBody PostRequest request) {
+        return new ResponseEntity<>(postService.createPost(request, user), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
@@ -26,12 +31,13 @@ public class PostController {
     }
 
     @PutMapping( "/{id}")
-    public ResponseEntity<PostResponse> updatePost(@PathVariable Long id, @RequestBody PostRequest request) {
-        return new ResponseEntity<>(postService.updatePost(id, request), HttpStatus.OK);
+    public ResponseEntity<PostResponse> updatePost(@AuthenticationPrincipal UserDetailsImpl user,
+                                                   @PathVariable Long id, @RequestBody PostRequest request) {
+        return new ResponseEntity<>(postService.updatePost(id, request, user), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public void deletePost(@PathVariable Long id) {
-        postService.deletePost(id);
+    public void deletePost(@AuthenticationPrincipal UserDetailsImpl user, @PathVariable Long id) {
+        postService.deletePost(id, user);
     }
 }
