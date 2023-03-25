7923bd611d9bdde4b856f9b3463c09f6d0a1ca66
kys0411 <32180846@dankook.ac.kr>
2023-03-25 14:08:03 +0900

    add: response dto에 속성 추가
    post, createdAt, updatedAt 추가

--- src/main/java/dku/merona/dto/RequestResponse.java
+++ src/main/java/dku/merona/dto/RequestResponse.java
@@ -2,8 +2,11 @@ package dku.merona.dto;
 
 import dku.merona.constant.RequestStatus;
 import dku.merona.domain.Member;
+import dku.merona.domain.Post;
 import dku.merona.domain.Request;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Getter
 public class RequestResponse {
@@ -16,13 +19,22 @@ public class RequestResponse {
 
     private Member member;
 
+    private Post post;
+
     private RequestStatus status;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 
     public RequestResponse(Request request) {
         this.id = request.getId();
         this.currentLocation = request.getCurrentLocation();
         this.dueTime = request.getDueTime();
         this.member = request.getMember();
+        this.post = request.getPost();
         this.status = request.getStatus();
+        this.createdAt = request.getCreatedAt();
+        this.updatedAt = request.getUpdatedAt();
     }
 }
