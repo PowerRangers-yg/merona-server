1ea426dbd162d0472ce23d7f6b653e070ef03de2
kys0411 <32180846@dankook.ac.kr>
2023-03-25 14:10:39 +0900

    add: 모든 메서드에 Authentication 어노테이션 추가

--- src/main/java/dku/merona/api/RequestController.java
+++ src/main/java/dku/merona/api/RequestController.java
@@ -1,11 +1,13 @@
 package dku.merona.api;
 
+import dku.merona.config.UserDetailsImpl;
 import dku.merona.dto.RequestRequest;
 import dku.merona.dto.RequestResponse;
 import dku.merona.service.RequestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,21 +20,25 @@ public class RequestController {
     private final RequestService requestService;
 
     @PostMapping("/new")
-    public ResponseEntity<RequestResponse> createRequest(@RequestBody RequestRequest request) {
-        return new ResponseEntity<>(requestService.createRequest(request), HttpStatus.OK);
+    public ResponseEntity<RequestResponse> createRequest(@AuthenticationPrincipal UserDetailsImpl user,
+                                                         @RequestBody RequestRequest request) {
+        return new ResponseEntity<>(requestService.createRequest(user, request), HttpStatus.OK);
     }
 
     @GetMapping("/{postId}")
-    public ResponseEntity<List<RequestResponse>> getAllRequestByPost(@PathVariable Long postId) {
-        List<RequestResponse> requestList = requestService.getAllRequestByPost(postId);
+    public ResponseEntity<List<RequestResponse>> getAllRequestByPost(@AuthenticationPrincipal UserDetailsImpl user,
+                                                                     @PathVariable Long postId) {
+        List<RequestResponse> requestList = requestService.getAllRequestByPost(user, postId);
         return new ResponseEntity<>(requestList, HttpStatus.OK);
     }
 
     @PatchMapping("/{id}")
-    public void cancelRequest(@PathVariable Long id) {requestService.cancelRequest(id);}
+    public void cancelRequest(@AuthenticationPrincipal UserDetailsImpl user,
+                              @PathVariable Long id) {requestService.cancelRequest(user, id);}
 
     @DeleteMapping("{id}")
-    public void deleteRequest(@PathVariable Long id) {
-        requestService.deleteRequest(id);
+    public void deleteRequest(@AuthenticationPrincipal UserDetailsImpl user,
+                              @PathVariable Long id) {
+        requestService.deleteRequest(user, id);
     }
 }
