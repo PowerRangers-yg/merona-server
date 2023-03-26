60497cb50c454ef3859f8507e482277ab8044782
kys0411 <32180846@dankook.ac.kr>
2023-03-26 15:16:59 +0900

    add: 멤버 검증 코드 추가

--- src/main/java/dku/merona/service/RequestService.java
+++ src/main/java/dku/merona/service/RequestService.java
@@ -1,11 +1,14 @@
 package dku.merona.service;
 
+import dku.merona.config.UserDetailsImpl;
+import dku.merona.domain.Member;
 import dku.merona.domain.Post;
 import dku.merona.domain.Request;
 import dku.merona.dto.RequestRequest;
 import dku.merona.dto.RequestResponse;
 import dku.merona.repository.RequestRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,33 +23,38 @@ public class RequestService {
 
     private final RequestRepository requestRepository;
     private final PostService postService;
+    private final MemberService memberService;
 
     public Request findRequestById(Long requestId) {
         return requestRepository.findById(requestId)
                 .orElseThrow(() -> new NoSuchElementException("해당 요청이 없습니다"));
     }
 
-    public RequestResponse createRequest(RequestRequest requestRequest) {
-        Post post = postService.findPostById(requestRequest.getPostId());
+    public RequestResponse createRequest(UserDetailsImpl user, RequestRequest request) {
+        Post post = postService.findPostById(request.getPostId());
+        Member member = user.getMember();
 
-        requestRequest.setPost(post);
-        Request request = requestRepository.save(requestRequest.toEntity());
-        return new RequestResponse(request);
+        request.setMemberAndPost(member, post);
+        Request newRequest = requestRepository.save(request.toEntity());
+        return new RequestResponse(newRequest);
     }
 
-    public List<RequestResponse> getAllRequestByPost(Long postId) {
+    public List<RequestResponse> getAllRequestByPost(UserDetailsImpl user, Long postId) {
         Post post = postService.findPostById(postId);
+        memberService.validateMember(user.getId(), post.getMember());
         return requestRepository.findAllByPost(post)
                 .stream().map(RequestResponse::new).collect(Collectors.toList());
     }
 
-    public void cancelRequest(Long requestId) {
+    public void cancelRequest(UserDetailsImpl user, Long requestId) {
         Request request = findRequestById(requestId);
+        memberService.validateMember(user.getId(), request.getMember());
         request.setStatusCancelled();
     }
 
-    public void deleteRequest(Long requestId) {
+    public void deleteRequest(UserDetailsImpl user, Long requestId) {
         Request request = findRequestById(requestId);
+        memberService.validateMember(user.getId(), request.getMember());
         request.deleteRelation();
         requestRepository.deleteById(requestId);
     }
