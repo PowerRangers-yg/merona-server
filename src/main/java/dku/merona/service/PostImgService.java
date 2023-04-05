1b3761b1f903d6a56d998396fd845a14979e8a4d
kys0411 <32180846@dankook.ac.kr>
2023-04-05 17:41:18 +0900

    feat: postImg create 기능 추가 [#19]

---
+++ src/main/java/dku/merona/service/PostImgService.java
@@ -0,0 +1,31 @@
+package dku.merona.service;
+
+import dku.merona.domain.Post;
+import dku.merona.dto.PostImgDto;
+import dku.merona.repository.PostImgRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class PostImgService {
+
+    private final PostImgRepository postImgRepository;
+    private final S3UploadService s3UploadService;
+
+    public void createPostImgList(List<MultipartFile> multipartFiles, Post post) {
+        List<String> fileList = s3UploadService.uploadImage(multipartFiles);
+
+        for (String fileName : fileList) {
+            String fileUrl = s3UploadService.getImagePath(fileName);
+            PostImgDto postImgDto = new PostImgDto(fileName, fileUrl, post);
+
+            postImgRepository.save(postImgDto.toEntity());
+        }
+    }
+}
