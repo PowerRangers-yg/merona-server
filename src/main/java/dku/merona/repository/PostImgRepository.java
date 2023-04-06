e2434cf20a113b545b11f4cda0a6152dcfb070c4
kys0411 <32180846@dankook.ac.kr>
2023-04-06 17:48:57 +0900

    add: findAllByPost 메서드 추가

--- src/main/java/dku/merona/repository/PostImgRepository.java
+++ src/main/java/dku/merona/repository/PostImgRepository.java
@@ -1,9 +1,15 @@
 package dku.merona.repository;
 
+import dku.merona.domain.Post;
 import dku.merona.domain.PostImg;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostImgRepository extends JpaRepository<PostImg, Long> {
+
+    List<PostImg> findAllByPost(Post post);
+
 }
